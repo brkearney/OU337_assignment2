0d774149ee7b9e450af521b900afe35127e27a77
Brianna Kearney <bkearney920@gmail.com>
2016-02-04 01:08:32 -0500

    Implemented Ninety_Nine_Bottles_Of_Beer() function.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,8 +1,15 @@
-public class Main {
+public class BeerSong {
 
-    public String Ninety_Nine_Bottles_Of_Beer()
+    public void Ninety_Nine_Bottles_Of_Beer(int count)
     {
-
+        if (count != 1)
+        {
+            System.out.println(count + "bottles of beer on the wall, " + count + " bottles of beer\nTake one down, pass it around, " + (count - 1) + " bottles of beer on the wall.");
+        }
+        else
+        {
+            System.out.println(count + "bottles of beer on the wall, " + count + " bottles of beer\nTake it down, pass it around, no more bottles of beer on the wall.");
+        }
     }
 
     public static void main(String[] args) {
